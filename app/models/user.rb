e577f7353461b5d4e08883a6f54eaa9d333f19a2
haleykelling <haley@kellings.com>
2020-07-06 17:11:09 -0600

    Add authenticate controller with login method. Return json with name and token on login.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
 
     validates :username, presence: true, uniqueness: true 
     validates :password, presence: true
+
+    def full_name
+        return "#{self.first_name} #{self.last_name}"
+    end
 end
