a56e0b12d0d07e2de5c80381326ef00a8e4b8da1
haleykelling <haley@kellings.com>
2020-07-08 14:57:44 -0600

    Add route to get saved books for a user. Render json of saved books organized by bookshelf.

--- app/models/saved_book.rb
+++ app/models/saved_book.rb
@@ -1,4 +1,22 @@
 class SavedBook < ApplicationRecord
   belongs_to :user
   belongs_to :book
+
+  def organize_by_bookshelf(array)
+    organized_hash = {
+      to_read: [],
+      reading: [],
+      finished_reading: []
+    }
+    array.each do |saved_book_instance|
+      if saved_book_instance.bookshelf == 1
+        organized_hash[:to_read] << saved_book_instance
+      elsif saved_book_instance.bookshelf == 2
+        organized_hash[:reading] << saved_book_instance
+      elsif saved_book_instance.bookshelf == 3
+        organized_hash[:finished_reading] << saved_book_instance
+      end
+    end
+    return organized_hash
+  end
 end
